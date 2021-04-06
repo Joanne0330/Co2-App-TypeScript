7a47802509ef449ee5f66595f4923f00f6a75227
Joanne Chen <joanne_r_chen@yahoo.com>
2021-04-07 00:04:01 +0100

    form works but api blocked by COR

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
+// import React from 'react';
+// import axios from 'axios';
 //components
 import Header from './components/Header';
 import SearchForm from './components/SearchForm';
 
 
 function App() {
+
+  // const handleSearchData = (place: string, date: string) => {
+  //   console.log(place)
+  //   console.log(date)
+
+
+  //     const data = await axios(`http://ladataverte.fr/api/1.0/data_points?id_indicators[]=1&id_indicators[]=4&type_place[]=${place}&from=${date}&to=${date}`)
+  //     console.log(data)
+
+  // };
+
   return (
     <div>
       <Header />
--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,33 +1,59 @@
+import React from 'react';
+import useEffect from 'react';
+import axios from 'axios';
 import CSS from 'csstype';
 
+// type Props = {
+//     searchData: (place: string, date: string) => void;
+// }
+
 const SearchForm = () => {
+    const [place, setPlace] = React.useState("");
+    const [date, setDate] = React.useState("");
+
+    const handleSearchData = async(place: string, date: string) => {
+        console.log(date)
+        console.log(place)
+        const header = {
+            "headers": {
+                // "Access-Control-Allow-Origin": "*",
+                "Content-Type": "text/csv; charset=UTF-8"
+            }
+        }
+        const data = await axios(`http://ladataverte.fr/api/1.0/data_points?id_indicators[]=1&id_indicators[]=4&type_place[]=${place}&from=${date}&to=${date}`, header)
+
+  
+        console.log('hello')
+        console.log(data)
+        setDate("")
+        setPlace("")
+    }
+
     return (
         <div>
-            <form
-                style={formStyle}
-            
+            <label>Search countries or cities: </label>
+            <select
+                style={inputStyle}
+                onChange={(e) => setPlace(e.currentTarget.value)}
             >
-                <label>Search countries or cities: </label>
-                <select
-                    style={inputStyle}
-                >
-                    <option>Please choose...</option>
-                    <option value="country">Countries</option>
-                    <option value="city">Cities</option>
-                </select>
-                <br />
-                <label>Type in a date:</label>
-                <input
-                    style={inputStyle}
-                    type="text"
-                    placeholder="format: YYYY-MM-DD..."
-                >
-                </input>
-                <button
-                    style={buttonStyle}
-                >Search</button>
-            </form>
-            
+                <option>Please choose...</option>
+                <option value="country">Countries</option>
+                <option value="city">Cities</option>
+            </select>
+            <br />
+            <label>Type in a date:</label>
+            <input
+                style={inputStyle}
+                type="text"
+                placeholder="format: YYYY-MM-DD..."
+                value={date}
+                onChange={(e) => setDate(e.currentTarget.value)}
+            >
+            </input>
+            <button
+                onClick={() => handleSearchData(place, date)}
+                style={buttonStyle}
+            >Search</button>
         </div>
     )
 }
