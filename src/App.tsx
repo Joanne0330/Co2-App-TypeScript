8d2c1d33f401717cdbb5b2b9c3197d8c671be612
Joanne Chen <joanne_r_chen@yahoo.com>
2021-04-09 15:02:23 +0100

    able to retrieve data for postcode and date

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import SearchForm from './components/SearchForm';
 
 
 function App() {
-  const [morning, setMorning] = React.useState([])
+  // const [morning, setMorning] = React.useState([])
 
   const handleSearchData = async(date: string): Promise<any> => {
     console.log(date)
@@ -17,11 +17,40 @@ function App() {
           "Accept": "application/json"
       }
     }
-    const data = await axios(`https://api.carbonintensity.org.uk/regional/intensity/${date}/fw24h`, headers);
-   
-    console.log(data.data)
-    console.dir(data.data[0])
-    // setMorning(data.data[15].regions)
+
+    const data = await axios(`https://api.carbonintensity.org.uk/regional/intensity/${date}/fw24h/postcode/SE13`, headers);
+
+    const generalData = data.data.data;
+    console.log(generalData)
+
+    //morning 7am
+    const morningData = data.data.data.data[15];
+    const morningDetails = data.data.data.data[15].generationmix;
+    console.log(morningData.from)
+    console.log(morningDetails)
+
+    //midday 12pm
+    const middayData = data.data.data.data[25];
+    const middayDetails = data.data.data.data[25].generationmix;
+    console.log(middayData.from)
+    console.log(middayDetails)
+
+
+    //evening 6pm
+    const eveningData = data.data.data.data[37];
+    const eveningDetails = data.data.data.data[37].generationmix;
+    console.log(eveningData.from)
+    console.log(eveningDetails)
+
+
+    //near midnight
+    const nightData = data.data.data.data[48];
+    const nightDetails = data.data.data.data[48].generationmix;
+    console.log(nightData.from)
+    console.log(nightDetails)
+
+    
+
 
 
   };
@@ -30,9 +59,8 @@ function App() {
     <div>
       <Header />
       <SearchForm searchData={handleSearchData}/>
-      {/* {morning.map((region: []) => (
-        <p>region.shortname</p>
-      ))} */}
+
+
     </div>
   );
 }
