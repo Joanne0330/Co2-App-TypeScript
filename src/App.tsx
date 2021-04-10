592b5f01689817a154fc29a9a67bb87cceb48681
Joanne Chen <joanne_r_chen@yahoo.com>
2021-04-10 11:50:47 +0100

    all states set

--- src/App.tsx
+++ src/App.tsx
@@ -5,54 +5,72 @@ import Header from './components/Header';
 import SearchForm from './components/SearchForm';
 
 
+export type generationMixType = {
+  fuel: string;
+  perc: number
+}
+
+// export type cityName = {
+//   shortname: string
+// }
+
+// export type postcode = {
+//   postcode: string
+// }
+
 function App() {
+  //general info
+  const [city, setCity] = React.useState("");
+  const [postcode, setPostcode] = React.useState("")
+
+  // timed info
+  const [morningGM, setMorningGM] = React.useState([] as generationMixType[]);
+  const [middayGM, setMiddayGM] = React.useState([] as generationMixType[]);
+  const [eveningGM, setEveningGM] = React.useState([] as generationMixType[]);
+  const [nightGM, setNightGM] = React.useState([] as generationMixType[]);
+
 
   const handleSearchData = async(postcode: string, date: string): Promise<any> => {
-    console.log(date)
-
-
     const headers ={
       "headers": {
           "Accept": "application/json"
       }
     }
-
     const data = await axios(`https://api.carbonintensity.org.uk/regional/intensity/${date}/fw24h/postcode/${postcode}`, headers);
 
+    // general info
     const generalData = data.data.data;
     console.log(generalData)
 
+    setCity(generalData.shortname)
+    setPostcode(generalData.postcode)
+
+
     //morning 7am
-    const morningData = generalData.data[15];
     const morningDetails = generalData.data[15].generationmix;
-    console.log(morningData.from)
-    console.log(morningDetails)
+    setMorningGM(morningDetails)
 
     //midday 12pm
-    const middayData = generalData.data[25];
     const middayDetails = generalData.data[25].generationmix;
-    console.log(middayData.from)
-    console.log(middayDetails)
-
+    setMiddayGM(middayDetails);
 
     //evening 6pm
-    const eveningData = generalData.data[37];
-    const eveningDetails = generalData.data[37].generationmix;
-    console.log(eveningData.from)
-    console.log(eveningDetails)
 
+    const eveningDetails = generalData.data[37].generationmix;
+    setEveningGM(eveningDetails);
 
     //near midnight
-    const nightData = generalData.data[48];
     const nightDetails = generalData.data[48].generationmix;
-    console.log(nightData.from)
-    console.log(nightDetails)
-
-    
-
-
-
+    setNightGM(nightDetails)
   };
+
+  console.log(city)
+  console.log(postcode)
+  console.log(morningGM)
+  console.log(middayGM)
+  console.log(eveningGM)
+  console.log(nightGM)
+
 
   return (
     <div>
